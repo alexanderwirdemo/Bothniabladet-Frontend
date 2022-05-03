0e7606474f76fa80181842dfca29b0de253a949e
Erdymus <hamfer-0@student.ltu.se>
2022-05-03 20:09:34 +0200

    Sökfunktion körbar på nyckelord

--- bothniabladet-frontend/src/app/components/archiveview/archiveview.component.ts
+++ bothniabladet-frontend/src/app/components/archiveview/archiveview.component.ts
@@ -1,27 +1,58 @@
-import { Component, OnInit } from '@angular/core';
-import { Options } from './optionsinterface';
-import { OPTIONS } from './searchoptions';
+import { Component, OnInit, ViewChild, ElementRef  } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { ApiService } from '../../services/api.service';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-archiveview',
   templateUrl: './archiveview.component.html',
   styleUrls: ['./archiveview.component.css']
 })
+
 export class ArchiveviewComponent implements OnInit {
 
-  options = OPTIONS;
+  searchString: String;
+  combined: String;
+  results: String;
+  baseUrl: String;
+  
+  searchForm: FormGroup;
 
-  constructor() { }
+  constructor(
 
-  choice?: Options;
-  onSelect(option: Options): void {
-  this.choice = option;
-}
+    private http: HttpClient,
+    private formBuilder: FormBuilder,
+    private _api: ApiService,
 
+  ) {}
 
-  test = 'Hej';
+  onFormSubmit(){}
+
+  makeSearch(){
+
+    this.baseUrl = "images/keyword/"
+    this.searchString = (<HTMLInputElement>document.getElementById("searchbar")).value;
+    this.combined = this.baseUrl.concat(this.searchString.toString())
+  
+
+    this._api.getTypeRequest(this.combined).subscribe((res: any) => {
+      {
+          const entries = Object.entries(res);
+          this.results = JSON.stringify(entries);
+          
+
+      }}, err => {
+      console.log(err);
+      alert("Nehej, du")
+
+      });
+  }
 
   ngOnInit(): void {
+
+    this.searchForm = this.formBuilder.group({})
+
   }
 
 }
