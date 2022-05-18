6de5df75c9baf670aaab1892e810e8c1bb4deff5
Alexander <alexander.wirdemo@gmail.com>
2022-05-18 18:16:26 +0200

    Removal, delete functionality, required fields

--- bothniabladet-frontend/src/app/components/customerview/customerview.component.ts
+++ bothniabladet-frontend/src/app/components/customerview/customerview.component.ts
@@ -27,7 +27,13 @@ export class CustomerviewComponent implements OnInit {
           this.imagesToReview.allImagesToReview[imageIndex]._id,
           "http://localhost:3001/uploaded_images/"+this.imagesToReview.allImagesToReview[imageIndex].title,
           this.imagesToReview.allImagesToReview[imageIndex].photographer,
-          this.imagesToReview.allImagesToReview[imageIndex].description
+          this.imagesToReview.allImagesToReview[imageIndex].description,
+          this.imagesToReview.allImagesToReview[imageIndex].date,
+          this.imagesToReview.allImagesToReview[imageIndex].Location.place,
+          this.imagesToReview.allImagesToReview[imageIndex].Location.city,
+          this.imagesToReview.allImagesToReview[imageIndex].Location.region,
+          this.imagesToReview.allImagesToReview[imageIndex].Location.country,
+          this.imagesToReview.allImagesToReview[imageIndex].Location.GPSCoordinates
         );
         this.imagesData.push(imageData);
       }
@@ -63,18 +69,48 @@ export class CustomerviewComponent implements OnInit {
     });
 } 
 
+declineImage(image: ImageData){
+  console.dir(image);
+  const id = image.id;
+  const body = {
+    _id: image.id,
+    reviewed: true
+  };
+  this._api.deleteTypeRequest('images/remove/'+image.id, body).subscribe(response => {
+    console.log(response);
+    this.removeElementFromObjectArray(image.id);
+    alert('Borttagen!');
+  }, er => {
+    console.log(er);
+    alert(er.error.error);
+  });
+    
+}
+
 }
 
 export class ImageData{
   public id: String;
   public filepath: String;
   public photographer: String;
-  public description: String
+  public description: String;
+  public date: String;
+  public place: String;
+  public city: String;
+  public region: String;
+  public country: String;
+  public GPScoordinates: String;
 
-  constructor(id: String, filepath: String, photographer: String, description: string){
+  constructor(id: String, filepath: String, photographer: String, description: String, date: String, place: String, city: String, region: String, country: String, GPScoordinates: String){
     this.id = id;
     this.filepath = filepath;
     this.photographer = photographer;
     this.description = description;
+    this.date = date;
+    this.place = place;
+    this.city = city;
+    this.region = region;
+    this.country = country;
+    this.GPScoordinates = GPScoordinates;
   }
 }
--- bothniabladet-frontend/src/app/components/imageview/imageview.component.ts
+++ bothniabladet-frontend/src/app/components/imageview/imageview.component.ts
@@ -98,9 +98,9 @@ export class ImageviewComponent implements OnInit {
     });
     this.location = this.formBuilder.group({
       GPS: "0,0",
-      place: 'Scandinavium',
-      city: 'Göteborg',
-      region: 'Västra Götaland',
+      place: '',
+      city: '',
+      region: '',
       country: 'Sverige'
     });
     this.technical_data = this.formBuilder.group({
@@ -178,15 +178,38 @@ export class ImageviewComponent implements OnInit {
 
 
   onFormSubmit() {
+    const imageData = this.image_data.value;
     console.log('image data form:');
-    console.dir(this.image_data.value);
+    console.dir(imageData);
+    const location = this.location.value;
     console.log('location form:');
-    console.dir(this.location.value);
+    console.dir(location);
+    const technical_data = this.technical_data.value;
     console.log('technical data form:');
-    console.dir(this.technical_data.value);
+    console.dir(technical_data);
+
+    if (imageData.photographer==="") {
+      alert('Fyll i fotograf');
+      return false;
+    }
+
+    if (imageData.description==="") {
+      alert('Fyll i beskrivning');
+      return false;
+    }
+
+    if (imageData.keywords==="") {
+      alert('Fyll i nyckelord');
+      return false;
+    }
+
+    if (location.place==="") {
+      alert('Fyll i plats');
+      return false;
+    }
 
     if (!this.fileUploadForm.get('uploadedImage').value) {
-      alert('Please fill valid details!');
+      alert('Välj bild att ladda upp!');
       return false;
     }
   
